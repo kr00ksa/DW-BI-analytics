a9b84743d8a3e45ff2f96c90d37a724c51d004b8
Veronika Nikiforova <116844717+kr00ksa@users.noreply.github.com>
2022-11-09 18:34:57 +0300

    Update Subqueries_LDB_2.sql

--- SQL tut/Subqueries_LDB_2.sql
+++ SQL tut/Subqueries_LDB_2.sql
@@ -16,3 +16,22 @@ SELECT
      LIMIT 1) product_example
 FROM category c 
 ORDER BY name
+
+-- Также задачу можно решить с помощью оконной функции 
+
+WITH t1 AS 
+(
+  SELECT
+      c.name AS name 
+    , p.name product_example
+    , RANK() OVER (PARTITION BY p.category_id ORDER BY p.name) rnk
+  FROM category c
+    LEFT JOIN product p
+      ON c.category_id = p.category_id
+)
+SELECT
+    name
+  , product_example
+FROM t1
+WHERE rnk = 1
+ORDER BY name
