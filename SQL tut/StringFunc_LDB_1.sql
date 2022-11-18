bb245e9b0ae9198dbffd11f1fdc933c308e17e2f
Veronika Nikiforova <116844717+kr00ksa@users.noreply.github.com>
2022-11-18 16:39:25 +0300

    Update StringFunc_LDB_1.sql

--- SQL tut/StringFunc_LDB_1.sql
+++ SQL tut/StringFunc_LDB_1.sql
@@ -10,3 +10,37 @@ SELECT
 FROM employee
 ORDER BY full_name
 
+/*https://learndb.ru/courses/task/79
+
+Задача
+	Объедини фамилию и имя через пробел из таблицы сотрудников employee. Выведи результат конкатенации в трех столбцах:
+	lower - строка, преобразованная в нижний регистр;
+	upper - строка, преобразованная в верхний регистр;
+	initcap - строка после применения функции initcap.
+	
+	Отсортируй результат сначала по фамилии, затем по имени.*/
+	
+SELECT
+    LOWER(CONCAT(last_name, ' ', first_name)) lower
+  , UPPER(CONCAT(last_name, ' ', first_name)) upper
+  , INITCAP(CONCAT(last_name, ' ', first_name)) initcap  
+FROM employee
+ORDER BY last_name, first_name
+
+/*https://learndb.ru/courses/task/86
+
+Задача
+	Получи строку с фамилией и первой буквой имени сотрудника в формате 'Фамилия И.'. Например, Иванов В. для Иванова Владимира. 
+	Выведи два столбца:
+	employee_id - идентификатор сотрудника;
+	full_name - фамилия с первой буквой имени.
+	
+	Отсортируй результат сначала по фамилии сотрудника, затем по имени.*/
+	
+SELECT
+    employee_id
+  , CONCAT(last_name, ' ', LEFT(first_name, 1), '.') full_name
+FROM employee
+ORDER BY last_name, first_name
+
+
