252463f007e8af4282a23d4bb37e064e52be249b
Veronika Nikiforova <116844717+kr00ksa@users.noreply.github.com>
2022-11-18 22:08:57 +0300

    Update StringFunc_LDB_1.sql

--- SQL tut/StringFunc_LDB_1.sql
+++ SQL tut/StringFunc_LDB_1.sql
@@ -11,24 +11,6 @@ FROM employee
 ORDER BY full_name
 
 
-/*https://learndb.ru/courses/task/79
-
-Задача
-	Объедини фамилию и имя через пробел из таблицы сотрудников employee. Выведи результат конкатенации в трех столбцах:
-	lower - строка, преобразованная в нижний регистр;
-	upper - строка, преобразованная в верхний регистр;
-	initcap - строка после применения функции initcap.
-	
-	Отсортируй результат сначала по фамилии, затем по имени.*/
-	
-SELECT
-    LOWER(CONCAT(last_name, ' ', first_name)) lower
-  , UPPER(CONCAT(last_name, ' ', first_name)) upper
-  , INITCAP(CONCAT(last_name, ' ', first_name)) initcap  
-FROM employee
-ORDER BY last_name, first_name
-
-
 /*https://learndb.ru/courses/task/86
 
 Задача
@@ -63,3 +45,56 @@ SELECT
   , LEFT(e.last_name, position('а' in LOWER(e.last_name))) AS substring2
 FROM employee
 ORDER BY last_name
+
+
+/*https://learndb.ru/courses/task/82
+
+Задача
+	Собери для каждого сотрудника из таблицы employee строку в формате "Фамилия ПерваяБукваИмени****". Звездочками заменяются все буквы имени, кроме первой.
+	Например, из 'Иванов Иван' должно получиться 'Иванов И***'.
+
+	В результате выведи поле:
+	mask - отформатированное ФИО сотрудника.
+	
+	Отсортируй результат по полю mask.*/
+	
+SELECT 
+    CONCAT(last_name, ' ', RPAD(LEFT(first_name, 1), LENGTH(first_name), '*')) mask
+FROM employee
+ORDER BY mask
+
+
+/*https://learndb.ru/courses/task/82
+
+Задача
+	Напиши запрос для получения из таблицы employee одного столбца:
+	full_name - ФИО сотрудника. Фамилия, имя и отчество должны быть разделены пробелом.
+	
+	У некоторых сотрудников не заполнено отчество. В full_name не должно быть пробелов в начале и конце строки.
+
+	Отсортируй результат по full_name.*/
+	
+SELECT RTRIM(CONCAT(last_name, ' ', first_name, ' ', middle_name)) full_name
+FROM employee
+ORDER BY full_name
+
+
+/*https://learndb.ru/courses/task/84
+
+Задача
+	В поле address таблицы store_address замени сокращения типа улицы на полное название:
+	ул. на улица;
+	пр. на проспект.
+	В результате запросы выведи один столбец:
+	address_full - получившаяся строка адреса.
+	
+	Отсортируй результат по address_full.*/
+	
+SELECT 
+    REPLACE(
+		REPLACE(
+			address, 'ул.', 'улица'
+		), 'пр.', 'проспект'
+	) address_full
+FROM store_address
+ORDER BY address_full
