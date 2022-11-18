5a2b852bb841cf8498f742bcdbdda1a59956941c
Veronika Nikiforova <116844717+kr00ksa@users.noreply.github.com>
2022-11-18 18:38:50 +0300

    Update StringFunc_LDB_1.sql

--- SQL tut/StringFunc_LDB_1.sql
+++ SQL tut/StringFunc_LDB_1.sql
@@ -10,6 +10,7 @@ SELECT
 FROM employee
 ORDER BY full_name
 
+
 /*https://learndb.ru/courses/task/79
 
 Задача
@@ -26,6 +27,7 @@ SELECT
   , INITCAP(CONCAT(last_name, ' ', first_name)) initcap  
 FROM employee
 ORDER BY last_name, first_name
+
 
 /*https://learndb.ru/courses/task/86
 
@@ -44,3 +46,20 @@ FROM employee
 ORDER BY last_name, first_name
 
 
+/*https://learndb.ru/courses/task/81
+
+Задача
+	Из фамилии сотрудника получи подстроку с начала и до первой буквы 'а' включительно. Буква 'а' может быть как заглавной, так и строчной.
+
+	Выведи столбцы:
+	last_name - фамилия сотрудника;
+	substring - часть фамилии до буквы 'а'.
+	
+	Отсортируй результат по фамилии.*/
+	
+SELECT
+    last_name
+  , SUBSTR(last_name, 1, POSITION('а' in LOWER(last_name))) AS substring1
+  , LEFT(e.last_name, position('а' in LOWER(e.last_name))) AS substring2
+FROM employee
+ORDER BY last_name
