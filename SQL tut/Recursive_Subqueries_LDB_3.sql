4d6119f62861455a5913a7a8db929bf3d127dd2c
Veronika Nikiforova <116844717+kr00ksa@users.noreply.github.com>
2022-11-26 00:04:44 +0300

    Update Recursive_Subqueries_LDB_3.sql

--- SQL tut/Recursive_Subqueries_LDB_3.sql
+++ SQL tut/Recursive_Subqueries_LDB_3.sql
@@ -107,3 +107,48 @@ SELECT
   , type
 FROM t1
 ORDER BY path_sort
+
+
+/*https://learndb.ru/courses/task/111
+
+Задача
+	Построй иерархию подчиненности сотрудников из таблицы employee.
+
+	В результате запроса выведи столбцы:
+		employee_id - идентификатор сотрудника;
+		full_name - имя и фамилия сотрудника, разделенные пробелом. Слева добавь 8 пробелов на каждый уровень вложенности. Перед ФИО сотрудника, которым никто не руководит, пробелов быть не должно.
+		is_manager - признак, что у сотрудника есть подчиненные (булево значение): TRUE - есть подчиненные, FALSE - нет подчиненных.
+		
+	Отсортируй результат так, чтобы:
+		подчиненные руководителя следовали сразу за строчкой с руководителем;
+		подчиненные одного руководителя были отсортированы сначала по имени, затем по фамилии;
+		сотрудники без руководителя отсортированы сначала по имени, затем по фамилии, затем по идентификатору сотрудника.*/
+
+
+WITH RECURSIVE hierarchy AS (
+SELECT
+    employee_id
+  , 1 AS count_managers
+  , CONCAT(first_name, ' ', last_name) AS full_name
+  , array[ROW_NUMBER() OVER (ORDER BY first_name, last_name, employee_id)] AS sort  
+FROM employee
+WHERE manager_id IS NULL
+
+UNION ALL
+
+SELECT
+    e.employee_id
+  , h.count_managers + 1 AS count_managers
+  , CONCAT(LPAD('', count_managers * 8, ' '), CONCAT(e.first_name, ' ', e.last_name)) AS full_name
+  , sort || ROW_NUMBER() 
+              OVER (PARTITION BY e.manager_id ORDER BY e.first_name, e.last_name, e.employee_id) AS sort  
+FROM hierarchy h
+  INNER JOIN employee e
+    ON e.manager_id = h.employee_id
+)
+SELECT
+      employee_id
+    , full_name
+    , count_managers < LEAD(count_managers, 1, count_managers) OVER (ORDER BY sort) AS is_manager
+FROM hierarchy
+ORDER BY sort
