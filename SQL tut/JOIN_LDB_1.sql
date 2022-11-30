3dd59f31fac1d0846bc8230d49386ddf760eaa26
Veronika Nikiforova <116844717+kr00ksa@users.noreply.github.com>
2022-11-30 19:17:51 +0300

    Update JOIN_LDB_1.sql

--- SQL tut/JOIN_LDB_1.sql
+++ SQL tut/JOIN_LDB_1.sql
@@ -29,3 +29,24 @@ FROM employee e
   INNER JOIN rank r
   ON e.rank_id = r.rank_id OR e.store_id = r.store_id
 ORDER BY last_name, first_name, store_id_rank, rank_id
+
+
+/*https://learndb.ru/courses/task/33
+
+Задача
+	Для каждой должности rank получи информацию о других должностях этого же магазина. 
+	Выведи:
+		store_id - идентификатор магазина;
+		rank_id - идентификатор должности;
+		rank_id_other - идентификатор другой должности магазина.
+		
+	Отсортируй результат по store_id, rank_id, rank_id_other.*/
+	
+SELECT
+    r1.store_id
+  , r1.rank_id
+  , r2.rank_id rank_id_other
+FROM rank r1
+  INNER JOIN rank r2
+    ON r1.store_id = r2.store_id AND r1.rank_id != r2.rank_id
+ORDER BY store_id, rank_id, rank_id_other
